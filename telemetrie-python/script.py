ad2d126b25f6f67f4004804f960a13741553121c
Baptiste Pauchet <baptistep@pc-fixe-baptiste.home>
2022-04-24 17:21:13 +0200

    a

--- telemetrie-python/script.py
+++ telemetrie-python/script.py
@@ -1,34 +1,11 @@
 import serial
-from tkinter import*
+import time
 
-screen =Tk()
-screen.title("test")
-screen.maxsize(500,500)
-screen.minsize(500,500)
+ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
+#ser.open()
 
-#label
-
-#var
-def nettoie(Liste):
-    data = []
-    for i in range(len(Liste)):
-        temp=Liste[i][2:]
-        data.append(temp[:-5])
-    return data
-def reception():
-    arduino=serial.Serial("/dev/ttyUSB0",timeout=1)
-    rawdata = []
-    cleandata = []
-    rawdata.append(str(arduino.readline()))
-    cleandata = nettoie(rawdata)
-    LabelVal = Label(text=cleandata).pack()
-    print(cleandata)
-    print(rawdata)
-    rawdata = []
-    cleandata = []
-
-reception()
-
-
-
-screen.mainloop()
+try:
+   serial_data = ser.readline()
+   print (serial_data)
+except serial.serialutil.SerialException:
+   pass
--- telemetrie/telemetrie.ino
+++ telemetrie/telemetrie.ino
@@ -1,12 +1,33 @@
-#include <SoftwareSerial.h>
+char i =0;
+char  a =0;
+char b=0;
 
-SoftwareSerial hc06(4, 3);
 
 void setup(){
-  Serial.begin(9600);
-  hc06.begin(9600);
+
+ Serial.begin(9600);
+ for(i=0;i<25;i++){
+
+    Serial.println('l');}
+    Serial.print('\n');
+    delay(2000);
 }
 
+
 void loop(){
-  hc06.write("Sa marche");
+
+ for(i=0;i<25;i++){
+     for(a=0;a<i;a++){
+      if((a==9)||(a==19)||(a==24))
+          Serial.println('l');
+      else
+          Serial.println('d');   
+     }
+     for(b=0;b<25-i;b++){
+          Serial.println('l');
+     }
+
+
+     delay(2000);
+  }
 }
